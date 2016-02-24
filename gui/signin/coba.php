6db716d0d8d0b86ee538da1e82bed788d6846fef
halimatuz <h52zzuhriyah@gmail.com>
2016-02-24 20:50:36 +0800

    do : memperbaiki gui signin coba.php

--- gui/signin/coba.php
+++ gui/signin/coba.php
@@ -10,66 +10,47 @@
 	body{
 		background-image: url("classroom.jpg");
 	}
-	div.col-sm4{
-		background-color: LightSteelBlue;
-		 opacity: 0.6;
+.col-sm-4{
+		background-color: white;
+		 opacity: 0.9;
     filter: alpha(opacity=60);
 	}
+.tengah{
+  margin: auto;
+     position: absolute;
+     top: 0; left: 0; bottom: 0; right: 0;
+     height: 300px;	
+ }
   </style>
   </head>
 
   <body >
   
 	<div class="header">
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-    
-	<br>
-	<br>
-	<br>
-	</div>
-	<div class="row"  >
-	<div class="col-sm-4" >
 
 	</div>
-		<div class="col-sm-4">
+	<div class="tengah">
+	<div class="row "  >
+	<div class="col-sm-4" ></div>
+	<div class="col-sm-4" style="padding:30px">
       <form class="form-horizontal" >
-        <h2 class="form-signin-heading" style="color:Black   ">Sistem Absensi Online</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus><br>
+        <h2 class="form-signin-heading text-center" style="color:Black   ">Sistem Absensi Online</h2>
+        <label for="NIM" class="sr-only">NIM</label>
+        <input type="text" id="inputNIM" class="form-control" placeholder="NIM" required autofocus><br>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required><br>
         <div class="checkbox">
           <label style="color:LightSteelBlue ">
             <input type="checkbox" value="remember-me" > Remember me
           </label>
-		<div></div>
-        </div>
+        </div><br>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-		<br>
-		<br>
       </form>
-	 
-		</div>
-    </div> <!-- /container -->
+	</div>
+    </div> 
+    </div><!-- /container -->
 <div class="footer">
-<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
+
 	</div>
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
