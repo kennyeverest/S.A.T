3a2f886b7a38dbe6ea0d0cf0c91b0a1cc1cab4b9
Kenny Karnama <kenny.everest.k-2014@fst.unair.ac.id>
2016-02-27 00:25:51 +0700

    do : fitur login dan input mk

---
+++ sat-fixed/application/views/sat/input/inputmk.php
@@ -0,0 +1,98 @@
+<html lang="en">
+<head>
+  <title>Bootstrap Case</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<style>
+	tengah{
+  margin: auto;
+     position: absolute;
+     top: 0; left: 0; bottom: 0; right: 0;
+     height: 300px;	
+ }
+ 
+ #test
+ {
+margin: auto;
+     position: absolute;
+     top: 0; left: 0; bottom: 0; right: 0;
+     height: 300px;	
+	}
+ #judul 
+ 	{
+ 		text-align:center;
+	}
+ 
+	</style>
+</head>
+<body>
+ 
+<div class="container " id="test" >
+  
+  <div class="panel panel-info">
+    <div class="panel-heading" id="judul">Input Mata Kuliah</div>
+    <div class="panel-body">
+    	<div class="header"></div>
+	
+	<div class="tengah">
+	<div class="row">
+	<div class="col-sm-4" ></div>
+	<div class="col-sm-4" style="padding:30px">
+   	<?php
+		$attribute = array( 'class' => 'form-horizontal',
+		       'role' => 'form');
+		echo form_open($aksi,$attribute);
+		?>
+	
+		<div class="form-group clearfix" >
+	
+		<input type="text" class="form-control" name="kodemk" id="kodemk" placeholder="Kode Mata Kuliah" required autofocus>
+		<br>
+		<input type="text" class="form-control" name="mk" id="mk" placeholder="Nama Mata Kuliah" required autofocus>
+		</label><br>
+		<div class="col-sm-4">
+		
+		</div>
+		<button class="btn btn-success btn-md" type="submit" >Simpan</button>
+		
+			
+		<div class="col-sm-3">
+		</div>
+		<button class="btn btn-danger btn-md" type="button" onclick="bersihkan()">Batal</button>	
+		
+		
+		
+	<?php form_close(); ?>
+		</div>
+	</div>
+	</div>
+	</div>
+    </div>
+  </div>
+  
+  <?php
+  
+  	if($flag==2)
+  	echo '<div class="alert alert-success">
+  <strong>Success!</strong> Data berhasil disimpan
+</div>';
+
+	else if($flag==1)
+	echo '<div class="alert alert-danger">
+  <strong>Error!</strong> Data tidak berhasil disimpan
+</div>'; 
+  
+  ?>
+<script>
+	function bersihkan()
+	{
+		document.getElementById('kodemk').value="";
+		document.getElementById('mk').value="";
+	}
+</script>
+
+</body>
+</html>
